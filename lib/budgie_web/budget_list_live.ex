1780112e28110a1c8d036da4797557d60e1d5cd0
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-12 22:22:09 +0700

    render budgets for loop with elixir heex

--- lib/budgie_web/budget_list_live.ex
+++ lib/budgie_web/budget_list_live.ex
@@ -1,13 +1,21 @@
 defmodule BudgieWeb.BudgetListLive do
   use BudgieWeb, :live_view
 
+  alias Budgie.Tracking
+
   def mount(_params, _session, socket) do
+    budgets = Tracking.list_budgets()
+    socket = assign(socket, budgets: budgets)
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
-    List the budgets here.
+    <ul>
+      <li :for={budget <- @budgets}>
+        {budget.name}
+      </li>
+    </ul>
     """
   end
 end
