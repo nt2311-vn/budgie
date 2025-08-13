2f41c94b7e0d4cb7db7a688f78339b230d016b68
nt2311-openSUSE <nguyentoan231196@gmail.com>
2025-08-13 21:24:46 +0700

    write tracking test just call valid_budget from the tracking fixtures

--- test/budgie/tracking_test.exs
+++ test/budgie/tracking_test.exs
@@ -1,6 +1,8 @@
 defmodule Budgie.TrackingTest do
   use Budgie.DataCase
 
+  import Budgie.TrackingFixtures
+
   alias Budgie.Tracking
 
   describe "budgets" do
@@ -9,19 +11,13 @@ defmodule Budgie.TrackingTest do
     test "create_budget/1 with valid data creates budgets" do
       user = Budgie.AccountsFixtures.user_fixture()
 
-      valid_attrs = %{
-        name: "some name",
-        description: "some description",
-        start_date: ~D[2025-01-01],
-        end_date: ~D[2025-03-31],
-        creator_id: user.id
-      }
+      valid_attrs = valid_budget_attributes(%{creator_id: user.id})
 
       assert({:ok, %Budget{} = budget} = Tracking.create_budget(valid_attrs))
       assert(budget.name == "some name")
       assert(budget.description == "some description")
       assert(budget.start_date == ~D[2025-01-01])
-      assert(budget.end_date == ~D[2025-03-31])
+      assert(budget.end_date == ~D[2025-01-31])
       assert(budget.creator_id == user.id)
     end
 
